b4976c1e6dc92ad2e03bd34c3198e49610014d59
Firewine <xyjworkgame@gmail.com>
2020-06-12 00:46:38 +0800

    scss 文件添加

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
-<div></div>
+<div id="app">
+    <router-view/>
+</div>
 </template>
 <script>
 
-import Storage from './store'
-export default {
-  name: 'app',
-  data(){
-    return{}
-  },
-  mounted () {
-    // Storage.setItem('a', 1);
-    // Storage.setItem('user', {a:1});
-    Storage.setItem('abcd', {a:1},'user');
 
-  }
+export default {
+  name: 'app'
+
+
 }
 </script>
 <style>
-
+/*@import "../src/assets/scss/config.scss";*/
 </style>
--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,5 +1,23 @@
 <template>
-<div>navHeader</div>
+<div class="header">
+    <div class="nav-topbar">
+        <div class="container">
+            <div class="topbar-menu">
+                <!--javascript: 页面不会刷新-->
+                <a href="javascript:;">小米商城</a>
+                <a href="javascript:;">MIUI</a>
+                <a href="javascript:;">云服务</a>
+                <a href="javascript:;">协议规则</a>
+            </div>
+            <div class="topbar-user">
+                <a href="javascript:;">登录</a>
+                <a href="javascript:;">注册</a>
+                <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+            </div>
+        </div>
+    </div>
+    <div class="nav-header"></div>
+</div>
 </template>
 
 <script>
@@ -8,6 +26,40 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    .header{
+        .nav-topbar{
+            height: 39px;
+            line-height: 39px;
+            background-color:#333333;
+            color: #B0B0B0;
+            .container{
+                width: 1226px;
+                margin-right: auto;
+                margin-left: auto;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                a{
+                    display: inline-block;
+                    color: #B0B0B0;
+                    margin-right: 17px;
+                }
+                .my-cart{
+                    width: 110px;
+                    background-color: #FF6600;
+                    text-align: center;
+                    color: #ffffff;
+                    .icon-cart{
+                        display: inline-block;
+                        width: 16px;
+                        height: 12px;
+                        background: url("../assets/imgs/icon-cart-checked.png") no-repeat center;
+                        background-size: contain;
+                        margin-right: 4px;
+                    }
+                }
+            }
+        }
+    }
 </style>
